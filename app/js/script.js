690de7f8b8155388fe58f9812cbdf44b901e4ac3
Pavel <pashagkjunost@gmail.com>
2019-06-01 19:25:44 +0300

    -start of working Version1.1.2

--- app/js/script.js
+++ app/js/script.js
@@ -1,38 +1,35 @@
 $(document).ready(function () {
-  let owl = $('.slider');
+  var owl = $('.slider_inside_design');
+  owl.owlCarousel();
+  // Go to the next item
+  $('.owl-next').click(function() {
+    owl.trigger('next.owl.carousel');
+  });
+  // Go to the previous item
+  $('.owl-prev').click(function() {
+    // With optional speed parameter
+    // Parameters has to be in square bracket '[]'
+    owl.trigger('prev.owl.carousel', [300]);
+  });
   owl.owlCarousel({
-    center: true,
-    items: 1,
+    center: false,
+    slideTransition: 'linear',
+    slideSpeed: 700,
+    autoPlay: 2500,
+    smartSpeed:700,
+    items: 3,
     loop: true,
     margin: 0,
     nav: true,
-    dots: true,
-    animateOut: 'fadeOut',
-    animateIn: 'fadeIn',
-    responsive: {
-      0: {
-        items: 1,
-        center: true,
-        autoplay: false,
-        autoplaySpeed: 500,
-        autoplayTimeout: 2000,
-        nav: true,
-      }
-    }
-  });
-  $('.slider_post').owlCarousel({
-    center: false,
-    items: 3,
-    loop: false,
-    margin: 0,
-    nav: true,
+    info: true,
+    onTranslate: callback,
     dots: false,
     responsive: {
       0: {
         items: 1,
         center: false,
         autoplay: false,
-        autoplaySpeed: 500,
+        autoplaySpeed: 900,
         autoplayTimeout: 2000,
         nav: true,
       },
@@ -40,46 +37,33 @@ $(document).ready(function () {
         items: 3,
         center: false,
         autoplay: false,
-        autoplaySpeed: 500,
-        autoplayTimeout: 2000,
-        nav: true,
-      }
-    }
-  });
-  $('.licence_slider').owlCarousel({
-    center: false,
-    items: 3,
-    loop: true,
-    margin: 0,
-    nav: true,
-    dots: false,
-    responsive: {
-      0: {
-        items: 1,
-        center: false,
-        autoplay: false,
-        autoplaySpeed: 500,
+        autoplaySpeed: 900,
         autoplayTimeout: 2000,
         nav: true,
       },
-      1000: {
-        items: 4,
-        center: false,
-        autoplay: false,
-        autoplaySpeed: 500,
-        autoplayTimeout: 2000,
-        nav: true,
-      }
     }
   });
   $('.mobile_menu').click(function(event) {
     $('.main_menu').toggleClass('active');
     $(this).toggleClass('active');
   });
-  $("#menu").on("click","a", function (event) {
-    event.preventDefault();
-    var id  = $(this).attr('href'),
-        top = $(id).offset().top;
-    $('body,html').animate({scrollTop: top}, 1500);
+  owl.on('changed.owl.carousel', function(event) {
+  callback(event);
   });
+  let owl2 = $('.designers_slider');
+  owl2.owlCarousel({
+    items: 1,
+  });
+
 });
+function callback(event) {
+  let itemsASD     = event.item.count;
+  let itemADS     = event.item.index;
+  let current = $('.count .current');
+  let all = $('.count .all');
+  // console.log(itemsASD);
+  // console.log(itemADS);
+  current.html('0'+itemADS);
+  all.html('0'+itemsASD);
+}
+
